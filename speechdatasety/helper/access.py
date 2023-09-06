5b27604d57368dc97720bea3621589970ff211f9
tarepan <tarepan5884@gmail.com>
2023-09-06 09:18:31 +0000

    Add PyTorch-based saver/loader

--- speechdatasety/helper/access.py
+++ speechdatasety/helper/access.py
@@ -3,21 +3,21 @@
 from pathlib import Path
 from typing import Callable, List, Tuple, Any, TypeVar, Union
 
-import numpy as np
-from numpy.typing import NDArray, ArrayLike
+import torch
 
-from .adress import generate_path_getter # pyright: ignore [reportMissingTypeStubs]
-from ..interface.speechcorpusy import ItemId     # pyright: ignore [reportMissingTypeStubs]
+from .adress import generate_path_getter     # pyright: ignore [reportMissingTypeStubs]
+from ..interface.speechcorpusy import ItemId # pyright: ignore [reportMissingTypeStubs]
 
 
-def save_np(path: Path, array: ArrayLike) -> None:
-    """Save numpy array in the path."""
+def save_pt(path: Path, obj: Any) -> None:
+    """Save item to the path."""
     path.parent.mkdir(parents=True, exist_ok=True)
-    np.save(path, array) # pyright: ignore [reportUnknownMemberType]
+    torch.save(obj, path)
 
-def load_np(path: Path) -> NDArray[Any]:
-    """Load numpy array in the path."""
-    return np.load(f"{str(path)}.npy") # pyright: ignore [reportUnknownMemberType, reportUnknownVariableType] ; because of numpy
+
+def load_pt(path: Path) -> Any:
+    """Load item in the path."""
+    return torch.load(path)
 
 
 NTuple = Union[
@@ -40,20 +40,20 @@ def generate_saver_loader(type_obj: I, names: List[str], root: Path) -> Tuple[Ca
     
     Args:
         type_obj - Example save/load target item for typing
-        names - Names of elements in the item
-        root - Dataset root adress
+        names    - Names of elements in the item
+        root     - Dataset root adress
     Returns:
-        save_nps - Save utility
-        load_nps - Load utility
+        _save    - Save utility
+        _load    - Load utility
     """
 
     get_path_funcs = [generate_path_getter(name, root) for name in names]
 
-    def save_nps(item_id: ItemId, items: I) -> None:
+    def _save(item_id: ItemId, items: I) -> None:
         for i, item in enumerate(items):
-            save_np(get_path_funcs[i](item_id), item)
+            save_pt(get_path_funcs[i](item_id), item)
 
-    def load_nps(item_id: ItemId) -> I:
-        return tuple(load_np(get_path(item_id)) for get_path in get_path_funcs) #type:ignore ; Hacking
+    def _load(item_id: ItemId) -> I:
+        return tuple(load_pt(get_path(item_id)) for get_path in get_path_funcs) #type:ignore ; Hacking
 
-    return save_nps, load_nps
+    return _save, _load
--- speechdatasety/helper/access_test.py
+++ speechdatasety/helper/access_test.py
@@ -3,39 +3,40 @@
 from pathlib import Path
 from typing import Tuple
 
-import numpy as np
-from numpy.typing import NDArray
+import torch
 
-from .access import generate_saver_loader, load_np, save_np
+from .access import generate_saver_loader, load_pt, save_pt
 from ..interface.speechcorpusy import ItemId     # pyright: ignore [reportMissingTypeStubs]
 
 
 def test_save_load(tmp_path: Path):
-    """Test `save_np` and `load_np`."""
+    """Test `save_pt` and `load_pt`."""
 
-    ary = np.array([1, 2, 3,], dtype=np.int64)
-    save_np(tmp_path, ary)
+    path = tmp_path / "aa"
+    ipt = torch.tensor([1, 2, 3,])
+    save_pt(path, ipt)
 
     # Simulate file closing & reopening
 
-    ary_reloaded = load_np(tmp_path)
+    ipt_reloaded = load_pt(path)
 
-    assert np.array_equal(ary, ary_reloaded), f"{ary} != {ary_reloaded}"
+    assert torch.equal(ipt, ipt_reloaded), f"{ipt} != {ipt_reloaded}"
+
 
 def test_generate_saver_loader(tmp_path: Path):
     """Test `generate_saver_loader`."""
 
-    subtype, speaker, uttr = "default", "spk_a", "num0"
-    item_id = ItemId(subtype, speaker, uttr)
+    corpus, subtype, speaker, uttr = "c1", "default", "spk_a", "num0"
+    item_id = ItemId(corpus, subtype, speaker, uttr)
 
-    THoge = NDArray[np.float32]                       # pylint: disable=invalid-name
-    THoge_: THoge = np.array([1.,], dtype=np.float32) # pylint: disable=invalid-name
-    TFuga = NDArray[np.float32]                       # pylint: disable=invalid-name
-    TFuga_: TFuga = np.array([1.,], dtype=np.float32) # pylint: disable=invalid-name
-    THogeFuga = Tuple[THoge, TFuga]                   # pylint: disable=invalid-name
-    THogeFuga_: THogeFuga = (THoge_, TFuga_)          # pylint: disable=invalid-name
+    THoge = torch.Tensor                     # pylint: disable=invalid-name
+    THoge_: THoge = torch.Tensor()           # pylint: disable=invalid-name
+    TFuga = torch.Tensor                     # pylint: disable=invalid-name
+    TFuga_: TFuga = torch.Tensor()           # pylint: disable=invalid-name
+    THogeFuga = Tuple[THoge, TFuga]          # pylint: disable=invalid-name
+    THogeFuga_: THogeFuga = (THoge_, TFuga_) # pylint: disable=invalid-name
 
-    hoge_fuga: THogeFuga = (np.array([1.,], dtype=np.float32), np.array([1., 2., 4.,], dtype=np.float32))
+    hoge_fuga: THogeFuga = ( torch.tensor([1.,]), torch.tensor([1., 2., 4.,]), )
 
     name_hoge, name_fuga = "hoge", "fuga"
     save, load = generate_saver_loader(THogeFuga_, ["hoge", "fuga"], tmp_path)
@@ -44,9 +45,9 @@ def test_generate_saver_loader(tmp_path: Path):
     hoge_fuga_reloaded = load(item_id)
 
     # File existance
-    assert (tmp_path / speaker / f"{name_hoge}s" / f"{uttr}.{name_hoge}.pt.npy").exists(), f"{name_hoge} npy not exists."
-    assert (tmp_path / speaker / f"{name_fuga}s" / f"{uttr}.{name_fuga}.pt.npy").exists(), f"{name_fuga} npy not exists."
+    assert (tmp_path / speaker / f"{name_hoge}s" / f"{uttr}.{name_hoge}.pt").exists(), f"{name_hoge} not exists."
+    assert (tmp_path / speaker / f"{name_fuga}s" / f"{uttr}.{name_fuga}.pt").exists(), f"{name_fuga} not exists."
 
     # Value euqality
-    assert np.array_equal(hoge_fuga[0], hoge_fuga_reloaded[0]), f"{hoge_fuga[0]} != {hoge_fuga_reloaded[0]}"
-    assert np.array_equal(hoge_fuga[1], hoge_fuga_reloaded[1]), f"{hoge_fuga[1]} != {hoge_fuga_reloaded[1]}"
+    assert torch.equal(hoge_fuga[0], hoge_fuga_reloaded[0]), f"{hoge_fuga[0]} != {hoge_fuga_reloaded[0]}"
+    assert torch.equal(hoge_fuga[1], hoge_fuga_reloaded[1]), f"{hoge_fuga[1]} != {hoge_fuga_reloaded[1]}"
